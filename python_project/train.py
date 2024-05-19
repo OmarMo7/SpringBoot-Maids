347ef261c6c0338bc0ed539e0a7ad9b64e372202
OmarMo7 <omar7.first@gmail.com>
2024-05-19 23:03:01 +0300

    Removing unnecesary imports

--- python_project/train.py
+++ python_project/train.py
@@ -1,15 +1,11 @@
 from sklearn.model_selection import GridSearchCV
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
 
+# Load train data
 data = pd.read_excel('../data/train.xlsx')
 
 
